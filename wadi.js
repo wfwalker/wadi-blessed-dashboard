a68bf5995826ea36b8df026fd3371e27ebcdd5e3
Bill Walker <bwalker@mozilla.com>
2015-10-16 11:56:07 -0700

    nodle dashboard for wadi

---
+++ wadi.js
@@ -0,0 +1,75 @@
+var request = require('request');
+
+var blessed = require('blessed');
+var contrib = require('blessed-contrib');
+
+var screen = blessed.screen();
+var grid = new contrib.grid({rows: 2, cols: 1, screen: screen});
+
+var line = grid.set(0, 0, 1, 1, contrib.line, {
+    style: {
+	line: "yellow",
+	text: "green",
+	baseline: "black"
+    },
+    xLabelPadding: 3,
+    xPadding: 5
+});
+
+var data = {
+    x: [],
+    y: []
+};
+
+for (var index = 0; index < 50; index++) {
+    data.x.push('t'+index);
+    data.y.push(Math.floor(Math.random() * 10));
+}
+
+screen.append(line);
+line.setData([data]);
+
+var bugList = grid.set(1, 0, 1, 1, contrib.table, {
+    keys: true,
+    fg: 'white',
+    label: 'wadi bugs',
+    columnWidth: [30, 30]
+});
+
+var bugListData = {
+    headers: ['id', 'title'],
+    data: [ ]
+};
+
+screen.append(bugList);
+bugList.setData(bugListData);
+
+screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+    return process.exit(0);
+});
+
+screen.render();
+
+request("http://bugzilla.mozilla.org/rest/bug/1201717", function(error, response, body) {
+    var tracker = JSON.parse(body);
+    var depends_on_list = tracker.bugs[0].depends_on;
+
+    for (var bugIndex in depends_on_list) {
+	
+	bugListData.data.push([depends_on_list[bugIndex], 'b']);
+    }
+    bugList.setData(bugListData);
+    screen.render();
+});
+
+
+setInterval(function() {
+    line.setLabel(new Date().toString());
+
+    data.y.shift();
+    data.y.push(Math.floor(Math.random() * 10));
+
+    line.setData([data]);
+
+    screen.render();
+}, 5000);
